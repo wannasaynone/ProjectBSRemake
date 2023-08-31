978250c1694a8c7a4926c3532ec3c51266c2d790
KAHA <wannasaynone@hotmail.com>
2023-08-31 08:09:58 +0800

    read test

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -2,28 +2,16 @@ using UnityEngine;
 
 public class GameStarter : MonoBehaviour
 {
+    [SerializeField] private TextAsset skillData;
+
+    public static KahaGameCore.GameData.Implemented.GameStaticDataManager GameStaticDataManager { get; private set; }
+
     private void Start()
     {
-        ProjectBS.Combat.CombatManager combatManager = new ProjectBS.Combat.CombatManager();
+        GameStaticDataManager = new KahaGameCore.GameData.Implemented.GameStaticDataManager();
 
-        ProjectBS.Combat.CombatActor player_actor1 = new ProjectBS.Combat.CombatActor(new ProjectBS.Combat.CombatActor.StatusInfo { Speed = 300 }) { name = "player_actor1" };
-        ProjectBS.Combat.CombatActor player_actor2 = new ProjectBS.Combat.CombatActor(new ProjectBS.Combat.CombatActor.StatusInfo { Speed = 200 }) { name = "player_actor2" };
-        ProjectBS.Combat.CombatActor player_actor3 = new ProjectBS.Combat.CombatActor(new ProjectBS.Combat.CombatActor.StatusInfo { Speed = 100 }) { name = "player_actor3" };
+        GameStaticDataManager.Add<ProjectBS.Data.SkillData>(new KahaGameCore.GameData.Implemented.GameStaticDataDeserializer().Read<ProjectBS.Data.SkillData[]>(skillData.text));
 
-        ProjectBS.Combat.CombatActor enemy_actor1 = new ProjectBS.Combat.CombatActor(new ProjectBS.Combat.CombatActor.StatusInfo { Speed = 270 }) { name = "enemy_actor1" };
-        ProjectBS.Combat.CombatActor enemy_actor2 = new ProjectBS.Combat.CombatActor(new ProjectBS.Combat.CombatActor.StatusInfo { Speed = 180 }) { name = "enemy_actor2" };
-        ProjectBS.Combat.CombatActor enemy_actor3 = new ProjectBS.Combat.CombatActor(new ProjectBS.Combat.CombatActor.StatusInfo { Speed = 90 }) { name = "enemy_actor3" };
-
-        combatManager.StartCombat(new System.Collections.Generic.List<ProjectBS.Combat.CombatActor>
-        {
-            player_actor1,
-            player_actor2,
-            player_actor3
-        }, new System.Collections.Generic.List<ProjectBS.Combat.CombatActor>
-        {
-            enemy_actor1,
-            enemy_actor2,
-            enemy_actor3
-        });
+        Debug.Log(GameStaticDataManager.GetGameData<ProjectBS.Data.SkillData>(1).Commands);
     }
 }
