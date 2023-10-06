700c35dfa5b15ba75206084dff365a63742521b5
KAHA <wannasaynone@hotmail.com>
2023-10-06 21:20:39 +0800

    add TargetSelector WIP

--- Assets/Scripts/Combat/TargetSelector.cs
+++ Assets/Scripts/Combat/TargetSelector.cs
@@ -8,6 +8,9 @@ namespace ProjectBS.Combat
     {
         private UI.SelectTargetMenu selectTargetMenu;
 
+        private List<CombatActor> player;
+        private List<CombatActor> enemy;
+
         public TargetSelector(UI.SelectTargetMenu selectTargetMenu)
         {
             this.selectTargetMenu = selectTargetMenu;
@@ -15,16 +18,58 @@ namespace ProjectBS.Combat
 
         public void SetSelectPool(List<CombatActor> player, List<CombatActor> enemy)
         {
-
+            this.player = player;
+            this.enemy = enemy;
         }
 
         public void StartSimpleSelect(IActor actor, string[] vars, Action<List<CombatActor>> onSelected)
         {
-
+            UnityEngine.Debug.Log(vars[0] + " " + vars[1]);
+            switch (vars[0])
+            {
+                case "Opponent":
+                    { 
+                        break;
+                    }
+                case "Ally":
+                    {
+                        break;
+                    }
+                case "All":
+                    {
+                        break;
+                    }
+                case "OtherAlly":
+                    {
+                        break;
+                    }
+                case "OtherAll": 
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        UnityEngine.Debug.Log("invaild select range: " + vars[0]);
+                        break;
+                    }
+            }
         }
 
         public void StartRandomSelect(IActor actor, string[] vars, Action<List<CombatActor>> onSelected)
         {
+            switch (vars[0])
+            {
+                case "Opponent": break;
+                case "Ally": break;
+                case "All": break;
+                case "OtherAlly": break;
+                case "OtherAll": break;
+                default:
+                    {
+                        UnityEngine.Debug.LogError("invaild select range: " + vars[0]);
+                        break;
+                    }
+            }
         }
     }
 }
